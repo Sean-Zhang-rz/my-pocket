60b2e83376e82693a11bed5136278997cf897599
seanZhang <770899447@qq.com>
2022-11-13 11:05:32 +0800

    overlayicon

---
+++ src/pages/OverlayIcon/index.tsx
@@ -0,0 +1,72 @@
+import { FC, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import styles from './index.module.scss';
+interface OverlayIconProps {
+  onClose: () => void;
+}
+export const OverlayIcon: FC<OverlayIconProps> = (props) => {
+  const nav = useNavigate();
+  const route = useParams();
+  useEffect(() => {}, []);
+  const onSignOut = async () => {
+    // await Dialog.confirm({
+    //   title: '确认',
+    //   message: '确认要退出登录吗？',
+    // });
+    localStorage.removeItem('jwt');
+    nav('/sign_in');
+  };
+  const close = () => {
+    props.onClose?.();
+  };
+  return (
+    <>
+      <div className={styles.mask} onClick={close}></div>
+      <div className={styles.overlay}>
+        <section className={styles.currentUser}>
+          {/* {myInfo ? (
+            <div>
+              <h2 className={styles.email}>{myInfo.email}</h2>
+              <p onClick={onSignOut}>退出登录</p>
+            </div>
+          ) : (
+            // <RouterLink to={`/sign_in?return_to=${route.fullPath}`}>
+            //   <h2>未登录用户</h2>
+            //   <p>点击这里登录</p>
+            // </RouterLink>
+          )} */}
+        </section>
+        <nav>
+          {/* <ul className={styles.action_list}>
+            <li>
+              <RouterLink to="/items/create" className={styles.action}>
+                <Icon name="pig" className={styles.icon} />
+                <span>记账</span>
+              </RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/statistics" className={styles.action}>
+                <Icon name="charts" className={styles.icon} />
+                <span>统计图表</span>
+              </RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/export" className={styles.action}>
+                <Icon name="export" className={styles.icon} />
+                <span>导出数据</span>
+              </RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/notify" className={styles.action}>
+                <Icon name="notify" className={styles.icon} />
+                <span>记账提醒</span>
+              </RouterLink>
+            </li>
+          </ul> */}
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default OverlayIcon;
