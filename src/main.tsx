6b59420ea8014f1a995c064799ddce39513d1aa0
sean Zhang <770899447@qq.com>
2022-11-14 18:58:12 +0800

    Revert "引入unocss"
    This reverts commit 27ba2b766ba266cace91bd162a65354c6b3a2895.

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './config/router';
 import './App.module.scss';
-import 'virtual:uno.css'
-
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
